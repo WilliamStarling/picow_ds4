ed8babef09fb018166e4b28563ceb886769b7859
WilliamStarling <williamjonas@comcast.net>
2025-04-16 00:45:18 -0500

    Works fully
    Outputs all the pressed buttons.

--- src/main.c
+++ src/main.c
@@ -158,7 +158,7 @@ void print_bits(uint8_t val) {
 #define BUTTON_PRESSED 1
 #define BUTTON_RELEASED -1
 #define BUTTON_HELD 0
-#define DEBOUNCE_TIME -11 //number of decrementations before the button is considered released. only works for odd numbers.
+#define DEBOUNCE_TIME -5 //number of decrementations before the button is considered released. only works for odd numbers.
 
 int buttonDebouncer(bool isPressed, int *buttonStatus) {
 	//printf("Button status: %d\n", *buttonStatus);
@@ -183,54 +183,6 @@ int buttonDebouncer(bool isPressed, int *buttonStatus) {
 		}
 	}
 	return BUTTON_HELD;
-}
-
-//Handles checking for each button press.
-void ButtonHandler(struct bt_hid_state state, struct buttonStatus *buttonsStatus) 
-{
-	//First, assign buttons their values.
-	uint8_t buttons = state.buttons;
-	uint8_t triggers = state.triggers;
-	uint8_t lx = state.lx;
-	uint8_t ly = state.ly;
-	uint8_t rx = state.rx;
-	uint8_t ry = state.ry;
-
-	/*
-	//First, print out the buttons:
-	printf("Face buttons: %02x, l: %d,%d, r: %d,%d, Trigger buttons: %02x",
-		buttons, lx, ly, rx, ry, triggers);
-	printf("\t Face Bits: ");
-	print_bits(buttons);
-	printf("\t Trigger Bits: ");
-	print_bits(triggers);
-	printf("\n");		
-	/*
-
-	//Check each button for if it's pressed.
-	/*starting with 1 in binary, and then shifting it over a certain ammount gets a byte
-	with just that bit on it's own. so using and & statement will return true if that bit
-	is on and false if not.*/
-	//Start with face buttons.
-	if(buttons & (1 << 4))
-	{ //SQUARE BUTTON CODE
-		if(buttonsStatus->square < -5) //only run this if the button had been released.
-		{
-			//Code for if Square / X button is pressed
-			printf("Square pressed\n");
-		}
-		buttonsStatus->square = 1; //Change this so code doens't get spam ran.
-	}
-	else if (!(buttons & (1 << 4)))
-	{
-		buttonsStatus->square--;
-		if (buttonsStatus->square == -5)
-		{  //once the button has been released for long enough, run released code.
-			//Code for if Square / X button is released	
-			//Stop doing whatever it square made it do.
-			printf("Square released\n");
-		}
-	}
 	/*
 	That probably seemed very messy- and will continue to seem so. The logic behind it is thus.
 	Soem of The buttons flicker between 1 and 0 when pressed, so I had to make sure it had been 0 for
@@ -239,69 +191,313 @@ void ButtonHandler(struct bt_hid_state state, struct buttonStatus *buttonsStatus
 	than -5 it runs the button pressed code. Then, when the button is off, it decreements  the status variable. 
 	Once it has been off for enough iterations, indicated by becoming -5, it runs the button released code.
 	*/
+}
 
-	if(buttons & (1 << 5)) {
+//Handles checking for each button press.
+void ButtonHandler(struct bt_hid_state state, struct buttonStatus *buttonsStatus) 
+{
+	//First, assign buttons their values.
+	uint8_t buttons = state.buttons;
+	uint8_t triggers = state.triggers;
+	/*The joysticks by default range from 0 - 255 (and in the case of the horizontal)
+	axis, 0 is all the way up). This code helps convert that from -127 to 128. 
+	Feel free to mess with as needed for your function calls. */
+	int ly = -state.ly + 128; //Center the joystick values to be between -127 and 128.
+	int lx = state.lx - 128; //Center the joystick values to be between -128 and 127.
+	int ry = -state.ry + 128; //Center the joystick values to be between -127 and 128.
+	int rx = state.rx - 128; //Center the joystick values to be between -128 and 127.
+
+	int stick_threshold = 10; //Threshold for joystick to be considered pushed. 10 is a good value, but can be changed.
+
+	//State variables for keeping track if the buttons were pressed, released, or being held.
+	int square_state = 0;
+	int ex_state = 0;
+	int circle_state = 0;
+	int triangle_state = 0;
+	int left_state = 0;
+	int down_state = 0;
+	int right_state = 0;
+	int up_state = 0;
+	int l1_state = 0;
+	int l2_state = 0;
+	int r1_state = 0;
+	int r2_state = 0;
+	int lJoy_state = 0;
+	int rJoy_state = 0;
+	int share_state = 0;
+	int options_state = 0;
+
+	/*
+	//First, print out the controller input:
+	printf("Face buttons: %02x, l: %d,%d, r: %d,%d, Trigger buttons: %02x",
+		buttons, lx, ly, rx, ry, triggers);
+	printf("\t Face Bits: ");
+	print_bits(buttons);
+	printf("\t Trigger Bits: ");
+	print_bits(triggers);
+	printf("\n");		
+	*/
+
+	//JOYSTICKS
+
+	if(ly > stick_threshold || ly < -stick_threshold) //LEFT JOYSTICK VERTICAL
+	{
+		//Code for if left joystick is moved vertically.
+		printf("left joystick moved vertically: %d\n", ly);
+	}
+	if(lx > stick_threshold || lx < -stick_threshold) //LEFT JOYSTICK HORIZONTAL
+	{
+		//Code for if left joystick is moved horizontally.
+		printf("left joystick moved horizontally: %d\n", lx);
+	}
+
+	if(ry > stick_threshold || ry < -stick_threshold) //RIGHT JOYSTICK VERTICAL
+	{
+		//Code for if right joystick is moved vertically.
+		printf("right joystick moved vertically: %d\n", ry);
+	}
+	if(rx > stick_threshold || rx < -stick_threshold) //RIGHT JOYSTICK HORIZONTAL
+	{
+		//Code for if right joystick is moved horizontally.
+		printf("right joystick moved horiontally: %d\n", rx);
+	}
+
+	//Check each button for if it's pressed.
+
+	//Dpad input uses a "Hat switch" encoding, so this code takes care of that. 2 alternatives are presented. but Hat switch looks like this:
+/*
+   0
+ 7   1
+6  8  2
+ 5   3
+   4
+*/
+
+//option 1: each direction is assigned an input, including diagonal, for a total of 8 individual actions.
+switch (buttons) {
+    case 0:
+		//Up code 
+		printf("Up pressed\n");
+		break;
+    case 1:   
+		//Up + Right code
+		printf("Up + Right pressed\n");
+		break;
+    case 2:
+		//Right code
+		printf("Right pressed\n");
+		break;
+    case 3:
+		//Down + Right code
+		printf("Down + Right pressed\n");
+		break;
+    case 4:
+		//Down code
+		printf("Down pressed\n");
+		break;
+    case 5:
+		//Down + Left code
+		printf("Down + Left pressed\n");
+		break;
+    case 6:
+		//Left code
+		printf("Left pressed\n");
+		break;
+    case 7:
+		//Up + Left code
+		printf("Up + Left pressed\n");
+		break;
+    case 8:
+		//Centered, nothing pressed
+		break;
+}
+
+/*
+//Option 2: each button is assigned an individual function, that so 2 can be performed at once if both are pressed.
+	bool dpad_up    = (buttons == 0 || buttons == 1 || buttons == 7);
+	bool dpad_right = (buttons == 1 || buttons == 2 || buttons == 3);
+	bool dpad_down  = (buttons == 3 || buttons == 4 || buttons == 5);
+	bool dpad_left  = (buttons == 5 || buttons == 6 || buttons == 7);
+	if(dpad_up) //UP BUTTON
+	{
+		//Code for if up button is pressed
+		printf("up button pressed\n");
+	}
+	if(dpad_right) //RIGHT BUTTON
+	{
+		//Code for if right button is pressed
+		printf("right button pressed\n");
+	}
+	if(dpad_down) //DOWN BUTTON
+	{
+		//Code for if down button is pressed
+		printf("down button pressed\n");
+	}
+	if(dpad_left) //LEFT BUTTON
+	{
+		//Code for if left button is pressed
+		printf("left button pressed\n");
+	}
+*/
+
+	/*starting with 1 in binary, and then shifting it over a certain ammount gets a byte
+	with just that bit on it's own. so using and & statement will return true if that bit
+	is on and false if not.*/
+	//Start with shape buttons.
+	square_state = buttonDebouncer(buttons & (1 << 4), &buttonsStatus->square); //SQUARE BUTTON
+	if(square_state == BUTTON_PRESSED)
+	{
+		//Code for if Square / X button is pressed
+		printf("Square pressed\n");
+	}
+	else if(square_state == BUTTON_RELEASED)
+	{
+		//Code for if Square / X button is released	
+		//Stop doing whatever it square made it do.
+		printf("Square released\n");
+	}
+
+	ex_state = buttonDebouncer(buttons & (1 << 5), &buttonsStatus->ex); //EX BUTTON
+	if(ex_state == BUTTON_PRESSED) 
+	{
 		//Code for if X / A button is pressed
+		printf("Ex pressed\n");
+	} else if(ex_state == BUTTON_RELEASED) 
+	{
+		//Code for if X / A button is released	
+		//Stop doing whatever it square made it do.
+		printf("Ex released\n");
 	}
-	if(buttons & (1 << 6)) {
+
+	circle_state = buttonDebouncer(buttons & (1 << 6), &buttonsStatus->circle); //CIRCLE BUTTON
+	if(circle_state == BUTTON_PRESSED) 
+	{
 		//Code for if Circle / B button is pressed
+		printf("Circle pressed\n");
 	}
-	if(buttons & (1 << 7)) {
+	else if(circle_state == BUTTON_RELEASED) 
+	{
+		//Code for if Circle / B button is released	
+		//Stop doing whatever it square made it do.
+		printf("Circle released\n");
+	}
+
+	triangle_state = buttonDebouncer(buttons & (1 << 7), &buttonsStatus->triangle); //TRIANGLE BUTTON
+	if(triangle_state == BUTTON_PRESSED){
 		//Code for if Triangle / Y button is pressed
+		printf("Triangle pressed\n");
+	}
+	else if(triangle_state == BUTTON_RELEASED) 
+	{
+		//Code for if Triangle / Y button is released	
+		//Stop doing whatever it square made it do.
+		printf("Triangle released\n");
 	}
 
 	//TRIGGER BUTTONS
-	/*
-	if(triggers & (1 << 3)) //RIGHT TRIGGER
-	{ 
-		if(buttonsStatus->r2 < -5) //only runs the button pressed code if it has been released.
-		{
-			//Code for if right trigger is pulled
-			printf("right trigger pressed\n");
-		}
-		buttonsStatus->r2 = 1; //Change this so code doens't get spam ran.
-	}
-	else if (!(triggers & (1 << 3)))
+	l1_state = buttonDebouncer(triggers & (1 << 0), &buttonsStatus->l1); //LEFT BUMPER BUTTON
+	if(l1_state == BUTTON_PRESSED) 
 	{
-		buttonsStatus->r2 --;
-		if (buttonsStatus->r2 == -5) 
-		{  //once the button has been released for long enough, run released code.
-			//Code for if right trigger is released	
-			//Stop doing whatever it square made it do.
-			printf("right trigger released\n");
-		}
+		//Code for if left bumper is pressed
+		printf("left bumper pressed\n");
 	}
-	*/
-	if(buttonDebouncer(triggers & (1 << 3), &buttonsStatus->r2) == BUTTON_PRESSED)
+	else if(l1_state == BUTTON_RELEASED) 
 	{
-		//Code for if right trigger is pulled
-		printf("\t\t\tright trigger pressed\n");
-	}
-	else if(buttonDebouncer((triggers & (1 << 3)), &buttonsStatus->r2) == BUTTON_RELEASED)
-	{
-		//Code for if right trigger is released	
-		//Stop doing whatever it square made it do.
-		printf("\t\t\tright trigger released\n");
+		//Code for if left bumper is released	
+		//Stop doing whatever left bumper made it do.
+		printf("left bumper released\n");
 	}
 
-	if(triggers & (1 << 2)) //LEFT TRIGGER
-	{ 
-		if(buttonsStatus->l2 < -5) //only runs the button pressed code if it has been released.
-		{
-			//Code for if left trigger is pulled
-			printf("left trigger pressed\n");
-		}
-		buttonsStatus->l2 = 1; //Change this so code doens't get spam ran.
-	}
-	else if (!(triggers & (1 << 2)))
+	r1_state = buttonDebouncer(triggers & (1 << 1), &buttonsStatus->r1); //RIGHT BUMPER BUTTON
+	if(r1_state == BUTTON_PRESSED) 
 	{
-		buttonsStatus->l2 --;
-		if (buttonsStatus->l2 == -5) 
-		{  //once the button has been released for long enough, run released code.
-			//Code for if left trigger is released	
-			//Stop doing whatever it square made it do.
-			printf("left trigger released\n");
-		}
+		//Code for if right bumper is pressed
+		printf("right bumper pressed\n");
+	}
+	else if(r1_state == BUTTON_RELEASED) 
+	{
+		//Code for if right bumper is released	
+		//Stop doing whatever right bumper made it do.
+		printf("right bumper released\n");
+	}
+
+	l2_state = buttonDebouncer(triggers & (1 << 2), &buttonsStatus->l2); //LEFT TRIGGER
+	if(l2_state == BUTTON_PRESSED)
+	{ 
+		//Code for if left trigger is pulled
+		printf("left trigger pressed\n");
+	}
+	else if (l2_state == BUTTON_RELEASED)
+	{ 
+		//Code for if left trigger is released	
+		//Stop doing whatever left trigger made it do.
+		printf("left trigger released\n");
+	}
+
+	r2_state = buttonDebouncer(triggers & (1 << 3), &buttonsStatus->r2); //RIGHT TRIGGER
+	if(r2_state == BUTTON_PRESSED)
+	{
+		//Code for if right trigger is pulled
+		printf("right trigger pressed\n");
+	}
+	else if(r2_state == BUTTON_RELEASED)
+	{
+		//Code for if right trigger is released	
+		//Stop doing whatever right trigger made it do.
+		printf("right trigger released\n");
+	}
+
+	//EXTRA BUTTONS
+	share_state = buttonDebouncer(triggers & (1 << 4), &buttonsStatus->share); //SHARE BUTTON
+	if(share_state == BUTTON_PRESSED) 
+	{
+		//Code for if share button is pressed
+		printf("share button pressed\n");
+	}
+	else if(share_state == BUTTON_RELEASED) 
+	{
+		//Code for if share button is released	
+		//Stop doing whatever share button made it do.
+		printf("share button released\n");
+	}
+
+	options_state = buttonDebouncer(triggers & (1 << 5), &buttonsStatus->options); //OPTIONS BUTTON
+	if(options_state == BUTTON_PRESSED) 
+	{
+		//Code for if options button is pressed
+		printf("options button pressed\n");
+	}
+	else if(options_state == BUTTON_RELEASED) 
+	{
+		//Code for if options button is released	
+		//Stop doing whatever options button made it do.
+		printf("options button released\n");
+	}
+	
+	lJoy_state = buttonDebouncer(triggers & (1 << 6), &buttonsStatus->lJoy); //LEFT JOYSTICK BUTTON
+	if(lJoy_state == BUTTON_PRESSED) 
+	{
+		//Code for if right joystick is pressed
+		printf("left joystick pressed\n");
+	}
+	else if(lJoy_state == BUTTON_RELEASED) 
+	{
+		//Code for if right joystick is released	
+		//Stop doing whatever right joystick made it do.
+		printf("left joystick released\n");
+	}
+
+	rJoy_state = buttonDebouncer(triggers & (1 << 7), &buttonsStatus->rJoy); //LEFT JOYSTICK BUTTON
+	if(rJoy_state == BUTTON_PRESSED) 
+	{
+		//Code for if left joystick is pressed
+		printf("right joystick pressed\n");
+	}
+	else if(rJoy_state == BUTTON_RELEASED) 
+	{
+		//Code for if left joystick is released	
+		//Stop doing whatever left joystick made it do.
+		printf("right joystick released\n");
 	}
 }
 
